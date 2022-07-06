d37422983b9a2306769acea1a2d30a6e63a0a71a
priyankamathukiya-GWL <priyanka.mathukiya@goodworklabs.com>
2022-07-06 18:16:06 +0530

    testa

--- app/src/main/java/com/example/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -24,6 +24,8 @@ class FirstFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
+
+
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
 
